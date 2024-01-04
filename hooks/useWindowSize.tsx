12dfec79663467a7412dca9f22b60ac0b3dd3b4c
kommade <99523586+kommade@users.noreply.github.com>
2024-01-04 13:26:15 +0800

    ughhhhh

--- hooks/useWindowSize.tsx
+++ hooks/useWindowSize.tsx
@@ -1,21 +1,23 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 function useWindowSize() {
   const [windowSize, setWindowSize] = useState({
     width: typeof window !== 'undefined' ? window.innerWidth : 0,
   });
 
+  const windowRef = useRef(window);
+
   useEffect(() => {
     function handleResize() {
-      setWindowSize({ width: window.innerWidth });
+      setWindowSize({ width: windowRef.current.innerWidth });
     }
 
-    window.addEventListener('resize', handleResize);
+    windowRef.current.addEventListener('resize', handleResize);
     handleResize();
     // TODO: add an update when the page is first loaded
 
-    return () => window.removeEventListener('resize', handleResize);
-  }, [window]);
+    return () => windowRef.current.removeEventListener('resize', handleResize);
+  });
 
   return windowSize;
 }
