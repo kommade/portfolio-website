41c394bf804b542779a1e074ecccf0a54031b307
kommade <99523586+kommade@users.noreply.github.com>
2024-02-14 18:02:31 +0800

    whoops

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://julittekhoo.com/',
+            url: 'https://juliettekhoo.com/',
             lastModified: new Date(),
             changeFrequency: 'daily',
             priority: 1,
