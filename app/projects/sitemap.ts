345eaec968846aebe4dff26c3af1961ea7fde2c0
kommade <99523586+kommade@users.noreply.github.com>
2024-02-14 18:36:18 +0800

    3rd time is the charm?

--- app/projects/sitemap.ts
+++ app/projects/sitemap.ts
@@ -5,7 +5,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
         const keys = await getAllProjects()
         const projects = await Promise.all(keys.map((key) => getProjectId(key)));
         return projects.map((project) => ({
-                url: `juliettekhoo.com/projects/${project.data}`,
+                url: `https://juliettekhoo.com/projects/${project.data}`,
                 lastModified: new Date()
         }))
 }
